adec4cb9d09adacd1be591f94aaf616e2cb30be2
ednaakpene <edvalerie18@gmail.com>
2024-04-05 16:23:42 +0000

    linked a button

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -4,7 +4,7 @@ export default function Navbar() {
   return (
     <AppBar color="success" position="static">
       <Toolbar>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+        <Typography to="/recipes" variant="h6" component={Link} sx={{ flexGrow: 1 }}>
           Recipe App
         </Typography>
         <Button to="/add-recipe" component={Link} variant="contained" color="success"> Add New Recipe</Button>
